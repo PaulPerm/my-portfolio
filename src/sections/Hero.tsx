a0da65bff4b7d8ac81b251b6e8e092c1fb23f67e
PaulPerm <paulpermyashkin17@gmail.com>
2026-05-14 13:45:02 -0400

    removed unused code

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -9,26 +9,7 @@ const arduinoModel = '/models/microchip.glb'
 const duckModel = '/models/free_rubber_duck_3d_model.glb'
 const sketchPadModel = '/models/sketchbook.glb'
 
-function OrbitTrail({ radius, tilt }: { radius: number; tilt: THREE.Euler }) {
-  const points: THREE.Vector3[] = []
-  for (let i = 0; i <= 128; i++) {
-    const angle = (i / 128) * Math.PI * 2
-    points.push(new THREE.Vector3(
-      Math.cos(angle) * radius,
-      0,
-      Math.sin(angle) * radius
-    ))
-  }
-  const geometry = new THREE.BufferGeometry().setFromPoints(points)
 
-  return (
-    <group rotation={tilt}>
-      <line geometry={geometry}>
-        <lineBasicMaterial color="#5c7a5e" opacity={0.15} transparent />
-      </line>
-    </group>
-  )
-}
 
 function Laptop({ mouse }: { mouse: { x: number; y: number } }) {
   const { scene } = useGLTF(laptopModel)
